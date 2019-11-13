8a6e5b3f91fe0ef4dafb4f98ad2bf2255314a9b4
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-13 19:10:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 doc.css(".headline-260IBN").text
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G.")
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
