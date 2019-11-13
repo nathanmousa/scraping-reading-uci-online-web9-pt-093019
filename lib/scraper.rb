6a3b1e34a5f513de583d24a6b2c970088248ed92
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-13 19:20:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,3 +9,5 @@ doc.css(".headline-260IBN").text
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
 courses.each do |course|
+  puts course.text.strip
+end
